59909e52a1495db1b226067ed9a6081c0ab10c4e
enochhz <walkerhaozheng@gmail.com>
2020-09-08 17:43:33 -0700

    Update express demo

--- 7-nodejs/express_demo.js
+++ 7-nodejs/express_demo.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use(express.json()); // json parser for post request
 const port = 8080
 
 app.get('/home', function(req, res) {
@@ -10,6 +11,19 @@ app.get('/json', function(req, res) {
     const data = require('./data.json'); 
     res.json(data);
 });
+
+app.get('/parameters', function(req, res) {
+    const head_info = req.query.head;
+    const para_info = req.query.para;
+    head_html = '<h1>' + head_info + '</h1>' 
+    paragraph_html = '<p>' + para_info + '</p>' 
+    res.send(head_html + paragraph_html)
+})
+
+app.post('/handle', function(req, res) {
+    console.log(req.body);
+    res.json(req.body);
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
