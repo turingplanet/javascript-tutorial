49d52462ca7e2067e85f3e59d9c46a9804e4f94e
enochhz <walkerhaozheng@gmail.com>
2020-09-11 10:19:15 -0700

    Update nodejs homework

--- 7-nodejs/homework.js
+++ 7-nodejs/homework.js
@@ -1,12 +1,11 @@
+const fs = require('fs')
 var express = require('express');
 var app = express();
-app.use(express.json()); // json parser for post request
-const fs = require('fs')
+app.use(express.json()); // JSON parser for post request
 
 app.get('/json_file', (req, res) => {
     try {
-        const fileName = __dirname + "/" + req.query.name + '.json';
-        const data = require(fileName);
+        const data = require(__dirname + '/' + req.query.name + '.json');
         res.json(data);
     } catch (err) {
         console.error(err);
@@ -16,15 +15,15 @@ app.get('/json_file', (req, res) => {
 
 app.post('/json_file', (req, res) => {
     try {
-        const fileName = __dirname + "/" + req.query.name + '.json';
+        const fileName = __dirname + '/' + req.query.name + '.json';
         bodyData = req.body;
         fs.open(fileName, 'r', (err, fd) => {
             if (err) {
-                fs.writeFile(fileName, JSON.stringify(bodyData), (err) => { if (err) console.log(err); });
+                fs.writeFile(fileName, JSON.stringify(bodyData), (err) => { if (err) console.log(err); }); // Create new file
             } else {
-                let fileContent = JSON.parse(fs.readFileSync(fileName, 'utf8')); // Read file content
+                let fileContent = JSON.parse(fs.readFileSync(fileName, 'utf8')); // Read file content 
                 Object.keys(bodyData).forEach( (key) => {fileContent.key = bodyData[key];});
-                fs.writeFileSync(fileName, JSON.stringify(fileContent)); // Write file content
+                fs.writeFileSync(fileName, JSON.stringify(fileContent)); // Write content to the file
             }
         })
         res.send({'success': 'File successfully updated.'})
@@ -36,15 +35,14 @@ app.post('/json_file', (req, res) => {
 
 app.delete('/json_file', (req, res) => {
     try {
-        const fileName = __dirname + "/" + req.query.name + '.json';
-        fs.unlinkSync(fileName);
+        fs.unlinkSync(__dirname + "/" + req.query.name + '.json');
         res.send({'success': 'File deleted.'})
     } catch (err) {
         console.log(err);
+        res.send({'error': 'Delete file failed.'})
     }
 });
 
-const port = 8080
-app.listen(port, () => {
+app.listen(8080, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
